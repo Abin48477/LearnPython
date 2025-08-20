d18887ea98aeb250added91faa7cbb4173b36576
Abin Ghimire <ghimireabin56@gmail.com>
2025-08-20 11:41:34 +0545

    Update Level2.py

--- Level2.py
+++ Level2.py
@@ -1,10 +1,38 @@
-#Exception Handling
-a = int(input("Enter numerator value:"))
-b = int (input("Enter denominator value:"))
-try:
-    r=a/b
-    print("Result is :",r)
-except:
-    print("division by zoro is not possible")
-finally:
-    print("This is the final code that should be run at any cost")
+# # #Exception Handling
+# # a = int(input("Enter numerator value:"))
+# # b = int (input("Enter denominator value:"))
+# # try:
+# #     r=a/b
+# #     print("Result is :",r)
+# # except:
+# #     print("division by zoro is not possible")
+# # finally:
+# #     print("This is the final code that should be run at any cost")
+    
+#     #switch case
+# day= int(input("Enter a number between 1 and 7:"))
+# match day:
+#     case 1:
+#         print("Sunday")
+#     case 2:
+#         print("Monday")
+#     case 3:
+#         print("Tuesday")
+#     case 4:
+#         print("Wedensday")
+#     case 5:
+#         print("Thursday")
+#     case 6:
+#         print("Friday")
+#     case 7:
+#         print("Saturday")
+#     case _:
+#         print("Invalid day number")
+
+#Tuple
+t = (1,'c',"lumbini",9+7)
+print(t)
+print(type(t))
+l = list(t)
+print(1)
+print(type(t))
