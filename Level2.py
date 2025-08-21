469feefa5e1d74cd40414824cd5897b9255dbb03
Abin Ghimire <ghimireabin56@gmail.com>
2025-08-21 14:54:16 +0545

    Update Level2.py

--- Level2.py
+++ Level2.py
@@ -45,6 +45,8 @@
 #     print("\r")
     
 #Table of 9
-for i in range(1,10):
-   print(f"{7} * {i} =",7*i)
+for i in range(1,21):
+   for j in range(1,11):
+    print(f"{i} * {j} =",i*j)
 
+#Triangle
