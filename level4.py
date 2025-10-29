a1b7a19ff4551ba3498c708b4a4eaa42990ffd71
Abin Ghimire <ghimireabin56@gmail.com>
2025-10-29 15:56:04 +0545

    updated

--- level4.py
+++ level4.py
@@ -1,3 +1,47 @@
-#class
-class Calculator:
-    def add(self,a,b)
+# Input from User
+# name = input("Enter your name:")
+# print("Hare Krishna",name)
+
+# age= int(input("Enter your age:"))
+# print(f"I am {age} years old.")
+
+# num1= int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# sum= num1+num2
+# print(f"The sum is: {sum}")
+
+# name=input("Enter your name ")
+# tea=40
+# snacks=40
+# totalBill=tea+snacks
+
+# print("Customer:",name)
+# print("Tea price is: ",tea)
+# print("Snacks Price is ",snacks)
+# print(f"Total Bill: RS. {totalBill}")
+# print("Thank you! Visit Again.")
+
+# age=int(input("Enter your age "))
+# if age<13:
+#     print("Child")
+# elif age >=13 and age <=19:
+#     print("Teenager")
+# else:
+#     print("Adult")
+
+# num=int(input("Enter any number:"))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("odd")
+
+username="abin"
+password ="krishna123"
+
+uname = input("Enter The Username: ")
+password1 =input("Enter The Password: ")
+
+if username == uname and password == password1:
+    print("Login Successful! Welcome,", uname)
+else:
+   print("Invalid username or password.") 
