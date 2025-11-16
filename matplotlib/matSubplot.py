ac70c128d29ccfcf3b90303281d00c1d379f6ad1
Abin Ghimire <ghimireabin56@gmail.com>
2025-11-16 22:19:59 +0545

    updated

---
+++ matplotlib/matSubplot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+# x= [1,2,3,4,7,11]
+# y1 =[1,2,3,3,4,4]
+# y2 =[3,4,5,6,7,5]
+# y3 = [1,3,5,7,9,6]
+
+# fig,axs = plt.subplots(2,2,figsize=(10,8))
+# #First Subplot
+
+# axs[0,0].plot(x,y1,marker='s',linestyle ='solid',color='green')
+# axs[0,0].set_title("TemperatureCensus")
+# axs[0,0].set_xlabel("Months")
+# axs[0,0].set_ylabel("Degrees")
+# #second Subplot
+# axs[0,1].plot(x,y1,marker='s',linestyle ='solid',color='green')
+# axs[0,1].set_title("Sales Census")
+# axs[0,1].set_xlabel("Months")
+# axs[0,1].set_ylabel("Sales amount")
+# #Third Sublot
+# axs[1,0].plot(x,y1,marker='s',linestyle ='solid',color='green')
+# axs[1,0].set_title("Health Census")
+# axs[1,0].set_xlabel("Months")
+# axs[1,0].set_ylabel("patient count")
+# #Forth Subplot
+# axs[1,1].plot(x,y1,marker='s',linestyle ='solid',color='green')
+# axs[1,1].set_title("Population Census")
+# axs[1,1].set_xlabel("Months")
+# axs[1,1].set_ylabel("patient count")
+
+
+# plt.tight_layout()
+# plt.show()
+
+#s
