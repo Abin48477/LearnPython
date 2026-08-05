6235f613419f9b7d9bcad9a22c73836449d0d7f6
Abin Ghimire <ghimireabin56@gmail.com>
2026-08-05 12:07:05 +0545

    Create hello.py

---
+++ Level1/hello.py
@@ -0,0 +1,8 @@
+print("Hello World!!")
+print("This is my first python program")
+
+print("hello","World!")#we use comma here python automatically add space  between each item.
+print('My favorite colors are', 'blue', 'green', 'red')
+
+# Output: My favorite colors are blue green red
+# Python automatically adds a space between each item when you separate them with commas. This is helpful when you want to print several pieces of information together.
