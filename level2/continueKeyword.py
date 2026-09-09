d9fbb8b657b2a3b12f7fd8645003766d81f5bbc5
Abin Ghimire <ghimireabin56@gmail.com>
2026-09-09 16:27:16 +0545

    Create continueKeyword.py

---
+++ level2/continueKeyword.py
@@ -0,0 +1,14 @@
+# it is use to skip the current iteration.for example:
+developer_name =['abin','prabin','sasin','ashok']
+
+for dn in developer_name:
+    if dn == 'prabin':
+        continue
+    print(dn)
+
+developer_names = ['Jess', 'Naomi', 'Tom']
+
+for developer in developer_names:
+    if developer == 'Naomi':
+        continue
+    print(developer)
