ae69203c3e5f55e43248c00c5998dfb5167a8a63
Abin Ghimire <ghimireabin56@gmail.com>
2026-09-18 16:36:02 +0545

    Create vsCodeDebugger.py

---
+++ level3/errorHandling/vsCodeDebugger.py
@@ -0,0 +1,14 @@
+def divide(a, b):
+    result = a / b
+    return result
+
+ans = divide(10, 2)
+print(ans)
+# Step 3: Start the debugger
+# Press:F5 or Run → Start Debugging
+
+# F5 → Continue ▶️
+# F10 → Step Over 👣next line
+# F11 → Step Into 🚪Open the function and go inside.
+# Shift + F11 → Step Out 🚪You've finished looking inside and want to go back to where the function was called.
+# get out of the function
